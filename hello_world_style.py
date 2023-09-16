894820c084c3cad293e752dc1d4bf4357b4b5041
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-09-16 18:08:17 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Julien Lamoureux
+# Date: September 16, 2023
+# This program displays "Hello World! with proper style." and "This is where I'm testing my function."
+
+
+def main():
+    print("Hello World! with proper style.")
+    print("This is where I'm testing my function.")
+
+
+if __name__ == "__main__":
+    main()
